ac3681f526da714a53399500bf684faf88a35131
Lautaro-Dominguez <lautaro.e.dominguez@gmail.com>
2022-06-10 20:20:12 -0300

    Desafio

---
+++ app.js
@@ -0,0 +1,6 @@
+const calcular = require("./calculadora")
+
+console.log( calcular.sumar(4, 2) )
+console.log( calcular.restar(4, 2) )
+console.log( calcular.multiplicar(4, 2) )
+console.log( calcular.dividir(4, 2) )
---
+++ calculadora.js
@@ -0,0 +1,8 @@
+
+
+module.exports= {
+    sumar: (nro1, nro2) => nro1 + nro2,
+    restar: (nro1, nro2) => nro1 - nro2,
+    multiplicar: (nro1, nro2) => nro1 * nro2,
+    dividir: (nro1, nro2) => nro1 / nro2
+    }
